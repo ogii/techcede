309150cdb1065ad5875175440f01c040e4868a5e
ogii <ordinallx@gmail.com>
2020-04-04 19:49:51 +0900

    Add smooth scrolling

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-// code for project goes here
+// update copyright year at bottom of page
+var current_year = new Date().getFullYear()
+$("#year_copyright").text(current_year);
+
+// implement smooth scroll
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top - 50
+  }, 500);
+});
